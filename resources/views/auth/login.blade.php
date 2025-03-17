ef06108a262968c9efa3d144c7401e7915e0d9f6
usmaniqbal2410 <usmaniqbal2410@gmail.com>
2025-03-17 16:05:01 +0500

    Done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -319,6 +319,7 @@
             <div class="container">
                 <div class="login-content">
                     <form method="POST" action="{{ route('login') }}" class="m-2">
+                        @csrf
                         <svg width="100" height="100" viewBox="0 0 48 48" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <g clip-path="url(#clip0_27_397)">
