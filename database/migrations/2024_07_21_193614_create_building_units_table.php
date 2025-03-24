58f2fca6e18c02f28cf4f2a222531bf289413072
Syed <syedmu729@gmail.com>
2025-03-24 15:21:41 +0500

    Notifications Done

--- database/migrations/2024_07_21_193614_create_building_units_table.php
+++ database/migrations/2024_07_21_193614_create_building_units_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->unsignedBigInteger('organization_id');
             $table->string('unit_name',50);
             $table->string('unit_type',50)->comment('Room, Shop, Apartment, Restaurant, Gym');
-            $table->enum('availability_status', ['Available', 'Rented', 'Sold', 'Not Available']);
+            $table->enum('availability_status', ['Available', 'Rented', 'Sold'])->default('Available');
             $table->enum('sale_or_rent', ['Sale', 'Rent', 'Not Available']);
             $table->decimal('price', 10, 2)->default(0);
             $table->decimal('area', 10, 2)->default(0);
