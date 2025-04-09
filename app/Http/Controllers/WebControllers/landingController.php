082e1168b47d9be75abcb7e4567c26506cbff4d5
Syed <syedmu729@gmail.com>
2025-04-09 19:45:51 +0500

    Notifications Done

--- app/Http/Controllers/WebControllers/landingController.php
+++ app/Http/Controllers/WebControllers/landingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\WebControllers;
 use App\Http\Controllers\Controller;
 use App\Models\Plan;
 use App\Models\PlanServicePrice;
+use Illuminate\Support\Facades\Log;
+
 
 class landingController extends Controller
 {
@@ -37,7 +39,33 @@ class landingController extends Controller
             }])
             ->get();
 
-        return response()->json(['plans' => $plans]);
+        Log::info('Plans : ' . $plans);
+
+        $organizedPlans = $plans->map(function ($plan) {
+            return [
+                'plan_id' => $plan->id,
+                'plan_name' => $plan->name,
+                'plan_description' => $plan->description,
+                'services' => $plan->services->map(function ($service) {
+                    return [
+                        'service_id' => $service->id,
+                        'service_name' => $service->name,
+                        'service_quantity' => $service->quantity,
+                        'prices' => $service->prices->map(function ($price) {
+                            return [
+                                'price_id' => $price->id,
+                                'amount' => $price->amount,
+                                'billing_cycle' => $price->billing_cycle,
+                            ];
+                        }),
+                    ];
+                }),
+            ];
+        });
+
+        Log::info('Plans : ' . $organizedPlans);
+
+        return response()->json(['plans' => $organizedPlans]);
     }
 
 }
