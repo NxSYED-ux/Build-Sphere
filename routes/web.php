a9c2c02a916eea7dd3ddfde3cf35693c2b707d11
Syed <syedmu729@gmail.com>
2025-04-24 00:58:10 +0500

    Notifications Done

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,8 @@ Route::prefix('checkout')->group(function () {
 
 Route::prefix('login')->group(function () {
 
-    Route::get('/login', [AuthController::class, 'index'])->name('login');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/', [AuthController::class, 'index'])->name('login');
+    Route::post('/', [AuthController::class, 'login'])->name('login');
 
 });
 
