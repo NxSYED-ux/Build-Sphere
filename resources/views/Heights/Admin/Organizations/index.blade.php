83ac2b56ffbc46a5713e8195ee919043bc33c0f6
usmaniqbal2410 <usmaniqbal2410@gmail.com>
2025-04-28 02:50:08 +0500

    Notifications Done

--- resources/views/Heights/Admin/Organizations/index.blade.php
+++ resources/views/Heights/Admin/Organizations/index.blade.php
@@ -1265,7 +1265,9 @@
                     title: 'Cannot Update',
                     text: 'Online payment cannot be enabled because the organization is not linked to any payment gateway.',
                     confirmButtonColor: '#3085d6',
-                    confirmButtonText: 'OK'
+                    confirmButtonText: 'OK',
+                    background: 'var(--body-background-color)',
+                    color: 'var(--sidenavbar-text-color)'
                 });
                 return;
             }
