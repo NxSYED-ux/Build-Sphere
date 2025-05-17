f9950974cec05d04c127b877f91d2a15f6b28680
Syed <syedmu729@gmail.com>
2025-05-17 23:42:39 +0500

    Notifications Done

--- database/migrations/2025_05_15_182209_create_membership_users_table.php
+++ database/migrations/2025_05_15_182209_create_membership_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('membership_user', function (Blueprint $table) {
+        Schema::create('membership_users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('membership_id')->constrained()->onDelete('cascade');
